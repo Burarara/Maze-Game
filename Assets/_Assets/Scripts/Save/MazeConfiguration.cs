32c7dab17b2352b979c8e7ab7d4862404ea32209
Burathat Junput <burathat.junput@radicalforge.com>
2023-05-22 09:59:35 +0100

    Initial Commit

---
+++ Assets/_Assets/Scripts/Save/MazeConfiguration.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeConfiguration
+{
+    public int Rooms;
+    public int Threats;
+    public int Treasures;
+}
