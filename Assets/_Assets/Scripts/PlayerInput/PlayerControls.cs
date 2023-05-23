b7bd9a1dfc1953581c47b05087ee430f966f2b28
Burathat Junput <burathat.junput@radicalforge.com>
2023-05-23 19:25:50 +0100

    Basica wall collision with player

--- Assets/_Assets/Scripts/PlayerInput/PlayerControls.cs
+++ Assets/_Assets/Scripts/PlayerInput/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-
+    [SerializeField] private LayerMask wallLayer;
     private void Move(Vector3 moveDir)
     {
         transform.position += moveDir;
@@ -21,7 +22,8 @@ public class PlayerControls : MonoBehaviour
     public void OnMoveHorizontal(InputValue value)
     {
         var input = value.Get<Vector2>();
-        if (input != Vector2.zero)
+        if (input != Vector2.zero && 
+            !Physics.Raycast(transform.position, input, 1f, wallLayer))
         {
             Move(input);
             Flip(input.x);
@@ -31,7 +33,8 @@ public class PlayerControls : MonoBehaviour
     public void OnMoveVertical(InputValue value)
     {
         var input = value.Get<Vector2>();
-        if (input != Vector2.zero)
+        if (input != Vector2.zero && 
+            !Physics.Raycast(transform.position, input, 1f, wallLayer))
         {
             Move(input);
         }
